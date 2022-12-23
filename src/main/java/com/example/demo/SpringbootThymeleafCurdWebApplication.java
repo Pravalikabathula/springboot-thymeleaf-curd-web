7e7d57f04e5f6ebedab0d2a5a90f4666e4149c26
Pravalikabathula <121026419+Pravalikabathula@users.noreply.github.com>
2022-12-23 12:23:30 +0530

    Update SpringbootThymeleafCurdWebApplication.java

--- src/main/java/com/example/demo/SpringbootThymeleafCurdWebApplication.java
+++ src/main/java/com/example/demo/SpringbootThymeleafCurdWebApplication.java
@@ -3,12 +3,19 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com")
 @EntityScan("com")
-public class SpringbootThymeleafCurdWebApplication {
+public class SpringbootThymeleafCurdWebApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder bulider) {
+		return bulider.sources(SpringbootThymeleafCurdWebApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootThymeleafCurdWebApplication.class, args);
